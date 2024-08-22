2a5b9159a7c3714e7e8d3925a5b2432f97570cc7
naza <npantanetti@asap-consulting.net>
2024-08-22 09:40:28 -0300

    creation of components and services

---
+++ src/app/gifs/components/gif-item/gif-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './gif-item.component.html',
+  styleUrl: './gif-item.component.css'
+})
+export class GifItemComponent {
+
+}
