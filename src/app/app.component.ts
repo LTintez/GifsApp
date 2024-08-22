d881a2c768e086448a77914a830232015489124c
naza <npantanetti@asap-consulting.net>
2024-08-22 11:16:41 -0300

    adding bootstrap styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './gifs/components/sidebar/sidebar.component';
+import { SearcherComponent } from './gifs/components/searcher/searcher.component';
+import { GifItemComponent } from './gifs/components/gif-item/gif-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    SidebarComponent
+    SidebarComponent,
+    SearcherComponent,
+    GifItemComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
